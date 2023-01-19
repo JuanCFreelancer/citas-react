fdf2603b92bcd838febdb2bc77e07e4ade9e0782
JuanCFreelancer <jcentenovillalobos@gmail.com>
2023-01-19 10:36:05 -0500

    Instalacion Tailwind CSS

---
+++ OneDrive/Escritorio/citas-react/src/App.jsx
@@ -0,0 +1,20 @@
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
+import ListadoPacientes from "./components/ListadoPacientes"
+
+Formulario
+
+function App() {
+
+  return (
+    <>
+
+      <Header />
+      <Formulario />
+      <ListadoPacientes/>
+
+    </>
+  )
+}
+
+export default App
